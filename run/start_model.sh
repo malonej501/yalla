57d7dd390f1320dc01033d0554f381901b481513
jmalone@hydra <jamesmalone@tutanota.com>
2024-09-30 15:33:54 +0100

    added annotations to start_model.sh

--- run/start_model.sh
+++ run/start_model.sh
@@ -1,6 +1,13 @@
 
 #!/bin/bash
 
+# Adding jobs to GPUs on hydra:
+# addqueue -q gpuQueueYouChose -s --gpus numberOfGPUsYouWant --gputype optionallySpecifyTheType -m CPURamYouNeedInGB./yourProgram
+# eg. addqueue -q gpulong -s --gpus 1 --gputype rtx2080with12gb ./myProgram
+# eg. addqueue -q gpulong -s -m 4 --gpus 1 ./myProgram
+# To look at GPUs active on hydra:
+# showgpus
+
 module load cuda
 nvcc ../examples/my_model.cu -o exec
 
