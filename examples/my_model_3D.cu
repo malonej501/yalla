428e9d550b614432e8fc4e265d2214bc35449796
jmalone@hydra <jamesmalone@tutanota.com>
2024-10-07 13:17:10 +0100

    implemented Volkening 2015 force equation with the differential adhesion and repulsion parameters used in the paper too. Cells seem to be sorting correctly, now need to implement proliferation and cell death.

--- examples/my_model_3D.cu
+++ examples/my_model_3D.cu
@@ -13,25 +13,38 @@
 #include "../include/utils.cuh"  
 #include "../include/vtk.cuh"
 
-const float r_max = 1.2;                        // Max distance betwen two cells for which they will interact
+const float init_dist = 0.1;                    // mean distance between cells when initialised
+const float r_max = 0.2;                        // Max distance betwen two cells for which they will interact
 const int n_0 = 500;                           // Initial number of cells
 const int n_max = 200000;                       // Max number of cells
-const float c_div = 0.005;                      // Probability of cell division per iteration
-const float noise = 0.5;                        // Magnitude of noise returned by generate_noise
+const float c_div = 0.0005;                      // Probability of cell division per iteration
+const float noise = 0; //0.5;                        // Magnitude of noise returned by generate_noise
 const float self_adh = 3.0;                     // Strength of adhesion
 const float non_self_rep = 4.0;                 // Strength of repulsion
 const float rep_ulim = 0.7;                      // The maximum distance between two cells for which they will repel
 const float adh_llim = 0.8;                     // The minimum distance between two cells for which they will atract
 
+// cell migration parameters
+const float rii = 0.02;                     // Length scales for migration forces for iri-iri (in mm)
+const float rxx = 0.011;                    // " xan-on-xan (in mm)
+const float rxi = 0.02;                     // " xan-on-iri (in mm)
+const float rix = 0.02;                     // " iri-on-xan repulsion (in mm)
+const float aix = 0.012;                    // " iri-on-xan attraction (in mm)
+const float Rii = 0.00124;                  // Repulsion from iri to iri (mm^2/day)
+const float Rxx = 0.00055;                  // Repulsion from xan to xan (mm^2/day)
+const float Rxi = 0.00274;                  // Repulsion force on iri due to xan (mm^2/day)
+const float Rix = 0.00226;                  // Repulsion force on xan due to iri (mm^2/day)
+const float Aix = 0.001956;                 // Attraction force on xan due to iri (mm^2/day)
 
-const int cont_time = 100;                  // Simulation duration in arbitrary time units 1000 = 40h ; 750 = 30h
+
+const int cont_time = 10000;                  // Simulation duration in arbitrary time units 1000 = 40h ; 750 = 30h
 const float dt = 0.1;                           // Time step for Euler integration
 
 // Macro that builds the cell variable type
 // MAKE_PT(Cell, u, v); // float3 i .x .y .z .u .v .whatever
 
 __device__ float* d_mechanical_strain; // define global variable for mechanical strain on the GPU (device)
-__device__ int* d_cell_type; // global variable for cell type on the GPU
+__device__ int* d_cell_type; // global variable for cell type on the GPU - iridophore=1, xanthophore=2
 __device__ float3* d_W; // global variable for random number from Weiner process for stochasticity
 
 template<typename Pt>
@@ -47,17 +60,57 @@ __device__ Pt pairwise_force(Pt Xi, Pt r, float dist, int i, int j)
     if (dist > r_max) return dF;
 
     // we define the strength of adhesion and repulsion
-    float k_adh = (d_cell_type[i] == d_cell_type[j]) ? self_adh : 1.0; // if the cell types are the same set adhesion to 3.0 if not then 1.0
-    float k_rep = (d_cell_type[i] == d_cell_type[j]) ? 1.0 : non_self_rep;
+    //float k_adh = (d_cell_type[i] == d_cell_type[j]) ? self_adh : 1.0; // if the cell types are the same set adhesion to 3.0 if not then 1.0
+    //float k_rep = (d_cell_type[i] == d_cell_type[j]) ? 1.0 : non_self_rep;
+    //float rep = (d_cell_type[i] == 1 and d_cell_type[j] == 1) ? rii
+    // float Adh = (d_cell_type[i] == 1 and d_cell_type[j] == 2) ? Aix : 0;
+    // float adh = (d_cell_type[i] == 1 and d_cell_type[j] == 2) ? aix : 0;
+    // float Rep = (d_cell_type[i] == 1 and d_cell_type[j] == 1) ? Rii : 0;
+    // float rep = (d_cell_type[i] == 1 and d_cell_type[j] == 1) ? rii : 0;
+
+    float Adh = 1;
+    float adh = 1;
+    float Rep = 1;
+    float rep = 1;
+
+    if (d_cell_type[i] == 1 and d_cell_type[j] == 1) { // iri - iri
+        Adh = 0;
+        adh = 1;
+        Rep = Rii;
+        rep = rii;
+    }
+    if (d_cell_type[i] == 1 and d_cell_type[j] == 2) { // iri - xan
+        Adh = Aix;
+        adh = aix;
+        Rep = Rix;
+        rep = rix;
+    }
+    if (d_cell_type[i] == 2 and d_cell_type[j] == 1) { // xan - iri
+        Adh = 0;
+        adh = 1;
+        Rep = Rxi;
+        rep = rxi;
+    }
+    if (d_cell_type[i] == 2 and d_cell_type[j] == 2) { // xan -xan
+        Adh = 0;
+        adh = 1;
+        Rep = Rxx;
+        rep = rxx;
+    }
 
 
-    float F = (k_rep * fmaxf(rep_ulim - dist, 0) - k_adh * fmaxf(dist - adh_llim, 0)); // forces are also dependent on adhesion and repulsion between cell types
-    // printf("%f\n", F);
+    //float F = (k_rep * fmaxf(rep_ulim - dist, 0) - k_adh * fmaxf(dist - adh_llim, 0)); // forces are also dependent on adhesion and repulsion between cell types
+    //float F = (Adh * r.x * exp(-sqrt(r.x^2 + r.y^2) / adh)) / (adh * sqrt(r.x^2 + r.y^2)) - (Rep * r.x * exp(-sqrt(r.x^2 - r.y^2) / rep) / (rep * sqrt(r.x^2 - r.y^2)));
+    // Volkening et al. 2015 force potential, function in terms of distance in n dimensions
+    float term1 = Adh/adh * expf(-dist / adh);
+    float term2 = Rep/rep * expf(-dist / rep);
+    float F = term1 - term2;
+    //printf("%f\n", F);
     d_mechanical_strain[i] += F; // mechanical strain is the sum of forces on the cell
 
-    dF.x += r.x * F / dist;
-    dF.y += r.y * F / dist;
-    dF.z += r.z * F / dist;
+    dF.x -= r.x * F / dist; //need to negative sum because of the way Volkening et al. force potentials are implemented
+    dF.y -= r.y * F / dist;
+    dF.z -= r.z * F / dist;
 
     return dF;
 }
@@ -118,7 +171,7 @@ int main(int argc, char const* argv[])
     
     Solution<float3, Gabriel_solver> cells{n_max, 50, r_max};
     *cells.h_n = n_0;
-    random_sphere(0.7, cells);
+    random_sphere(init_dist, cells);
 
     cells.copy_to_device();
 
@@ -155,7 +208,7 @@ int main(int argc, char const* argv[])
             cells.take_step<pairwise_force, friction_on_background>(dt, generic_function);    
         }
 
-        if(time_step % 1 == 0){
+        if(time_step % 100 == 0){
             cells.copy_to_host();
             mechanical_strain.copy_to_host();
             cell_type.copy_to_host();
