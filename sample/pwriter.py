ab02f94d42ea448ad301d180576ff9bd43345614
jmalone@hydra <jamesmalone@tutanota.com>
2025-03-21 19:38:56 +0000

    parameters are split into annotated groups in .h file

--- sample/pwriter.py
+++ sample/pwriter.py
@@ -13,10 +13,15 @@ def params_to_header(params: pd.DataFrame):
     with open("../params/params.h", "w") as f:
         f.write("#ifndef PARAMS_H\n#define PARAMS_H\n\n")
 
+        curr_group = None
         for _, row in params.iterrows():
             param = row["param"]
             type_ = row["type"]
             value = row["value"]
+            group = row["group"]
+            if group != curr_group: # split params into groups
+                f.write(f"\n// {group}\n")
+                curr_group = group
             f.write(f"const {type_} {param} = {value};\n")
 
         f.write("\n#endif")
