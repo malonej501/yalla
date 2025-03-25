d781203f0f27cd93aa6414e1f2dd625d27f1e1e0
jmalone@hydra <jamesmalone@tutanota.com>
2025-03-25 17:55:37 +0000

    pwriter now generates a C++ logging script adjusted to the parameters present in default.csv which provides functions to spot_finder.cu that allow writing parameter values to .csv

--- sample/pwriter.py
+++ sample/pwriter.py
@@ -18,20 +18,55 @@ def params_to_header(params: pd.DataFrame):
         for _, row in params.iterrows():
             param = row["param"]
             type_ = row["type"]
+            value = row["value"]
             group = row["group"]
             if group != curr_group: # split params into groups
                 f.write(f"\n\t// {group}\n")
                 curr_group = group
-            f.write(f"\t{type_} {param};\n")
+            f.write(f"\t{type_} {param} = {value};\n")
         
         f.write("};\n\n")
-        v_list = params["value"].astype(str).tolist()
-        v_str = "{" + ", ".join(v_list) + "}"
-        f.write(f"Pm h_pm = {v_str};\n")
-        f.write("\n#endif")
+        # v_list = params["value"].astype(str).tolist()
+        # v_str = "{" + ", ".join(v_list) + "}"
+        # f.write(f"Pm h_pm = {v_str};\n")
+        f.write("Pm h_pm;\n")
+        f.write("#endif")
+    
+def write_logging_instructions(params: pd.DataFrame):
+    """Write C++ code to log parameters for each step of spot_finder.
+    
+    Args:
+        params (pd.DataFrame): DataFrame containing parameters.
+        
+    Returns:
+        None
+    """
 
+    with open("../sample/logging.cpp", "w") as f:
 
+        f.write("#include <fstream>\n")
+        f.write("#include <string>\n")
+        f.write('#include "../params/params.h"\n\n')
+
+        f.write("void write_report_header(std::ofstream& file) {\n")
+        step_info_h = "walk_id, step, attempt, status, target, "
+        param_names = ", ".join(params["param"].astype(str).tolist())
+        header = step_info_h + param_names
+        f.write(f'\tfile << "{header}' + r'\n";' + '\n')
+        f.write("}\n\n")
+
+        f.write("void write_report_row(std::ofstream& file, int walk_id, "
+        "int i, int attempt, std::string status, std::string target, const "
+        "Pm& h_pm) {\n")
+        step_info_r = ' << walk_id << "," << i << "," << attempt << "," << ' \
+        'status << "," << target << "," << h_pm.'
+        h_pm_names = ' << "," << h_pm.' \
+        ''.join(params["param"].astype(str).tolist())
+        row = step_info_r + h_pm_names
+        f.write(f"\tfile {row} <<" +r'"\n";')
+        f.write("}\n\n")
 
 if __name__ == "__main__":
     params = pd.read_csv("../params/default.csv")
     params_to_header(params)
+    write_logging_instructions(params)
