a4f9e689750e9dda3fb5a8f5c6f23e4fa1ac4fef
malonej501 <jamesmalone@tutanota.com>
2025-05-28 11:54:26 +0100

    added new rectangle approximately to scale with adult anal fin size

--- inits/init_mesh.py
+++ inits/init_mesh.py
@@ -1,32 +1,48 @@
 import sys
 from vedo import *
 
-display = True
-extrude = True
-extrude_z = 0.1  # extrude distance in z direction
-format = 0  # 0: legacy vtk yalla compatible, 1: new vtk (from vedo)
-shape = 1  # 0: 2D fin shape, 1: 2D rectangular shape
+DISPLAY = True
+EXTRUDE = True
+FORMAT = 0  # 0: legacy vtk yalla compatible, 1: new vtk (from vedo)
+SHAPE = 2  # 0: 2D fin shape, 1: 2D rectangular shape,
+EXTRUDE_Z = 0.1 if SHAPE != 2 else 100  # extrude distance in z direction
 
-faces = []
-verts = []
+# 2: 2D rect with specific dimensions
 
-if shape == 0:
-    # 2D fin shape
-    verts = [(-1, 0.5, 0), (0.75, 0.5, 0), (0.75, -0.5, 0),
-             (0.2, -0.5, 0), (-1, 0.1, 0)]
-    faces = [[0, 1, 2, 3, 4]]
 
-if shape == 1:
-    # 2D rectangular shape
-    verts = [(-1, 0.5, 0), (1, 0.5, 0), (1, -0.5, 0), (-1, -0.5, 0)]
-    faces = [[0, 1, 2, 3]]
+def get_shape():
+    """
+    Returns the vertices and faces of the fin mesh based on the SHAPE 
+    variable.
+    """
+    verts, faces = [], []
+    if SHAPE == 0:
+        # 2D fin shape
+        verts = [(-1, 0.5, 0), (0.75, 0.5, 0), (0.75, -0.5, 0),
+                 (0.2, -0.5, 0), (-1, 0.1, 0)]
+        faces = [[0, 1, 2, 3, 4]]
+
+    if SHAPE == 1:
+        # 2D rectangular shape
+        verts = [(-1, 0.5, 0), (1, 0.5, 0), (1, -0.5, 0), (-1, -0.5, 0)]
+        faces = [[0, 1, 2, 3]]
+
+    if SHAPE == 2:
+        # 2D rectangular shape centered at (0,0,0) with specified dimensions
+        ap = 7500  # anterior-posterior length
+        pd = 2500  # proximal-distal height
+        verts = [(-ap/2, pd/2, 0), (ap/2, pd/2, 0),
+                 (ap/2, -pd/2, 0), (-ap/2, -pd/2, 0)]
+        faces = [[0, 1, 2, 3]]
+
+    return verts, faces
 
 
 def export_vtk_custom(verts, faces):
     """Export verts and faces manually to legacy vtk"""
 
     # Create the vtk file
-    with open(f"shape{shape}_mesh_{'3D' if extrude else '2D'}.vtk",
+    with open(f"shape{SHAPE}_mesh_{'3D' if EXTRUDE else '2D'}.vtk",
               "w", encoding="utf-8") as vtk_file:
 
         # Write the header
@@ -54,9 +70,9 @@ def export_vtk_custom(verts, faces):
 def extrude_mesh(verts):
     """Extrude 2D shape in the z direction by a specified distance mesh"""
 
-    # Extrude in the z direction by 0.1 units
-    verts_3d = [(x, y, z + extrude_z) for (x, y, z) in verts] + \
-        [(x, y, z - extrude_z) for (x, y, z) in verts]
+    # Extrude in the z direction
+    verts_3d = [(x, y, z + EXTRUDE_Z) for (x, y, z) in verts] + \
+        [(x, y, z - EXTRUDE_Z) for (x, y, z) in verts]
 
     # Create faces for the extruded mesh
     num_verts = len(verts)
@@ -80,28 +96,30 @@ if __name__ == "__main__":
     args = sys.argv[1:]
 
     # print parameters
-    print(f"display: {display}")
-    print(f"extrude: {extrude}")
-    print(f"extrude_z: {extrude_z}")
-    print(f"format: {format}")
-    print(f"shape: {shape}")
+    print(f"DISPLAY: {DISPLAY}")
+    print(f"EXTRUDE: {EXTRUDE}")
+    print(f"EXTRUDE_Z: {EXTRUDE_Z}")
+    print(f"FORMAT: {FORMAT}")
+    print(f"SHAPE: {SHAPE}")
 
-    if extrude:
-        verts, faces = extrude_mesh(verts)  # extrude in z-direction
+    v, f = get_shape()  # get the shape vertices and faces
+
+    if EXTRUDE:
+        v, f = extrude_mesh(v)  # extrude in z-direction
 
     # use vedo to triangulate the mesh - essential for yalla compatibility
-    f_mesh = Mesh([verts, faces])
+    f_mesh = Mesh([v, f])
     f_mesh = f_mesh.triangulate()
 
-    if display:
+    if DISPLAY:
         f_mesh.color("grey").linecolor("black")  # .alpha(0.5)
         plt = Plotter(axes=1)
         plt.add(f_mesh)
         plt.show(azimuth=-30, elevation=-10, interactive=False)
-        plt.screenshot(f"shape{shape}_mesh_{'3D' if extrude else '2D'}.png")
+        plt.screenshot(f"shape{SHAPE}_mesh_{'3D' if EXTRUDE else '2D'}.png")
         plt.interactive()
 
-    if format == 0:
+    if FORMAT == 0:
         export_vtk_custom(f_mesh.vertices, f_mesh.cells)
-    elif format == 1:
-        f_mesh.write(f"shape{shape}_mesh.vtk", binary=False)
+    elif FORMAT == 1:
+        f_mesh.write(f"shape{SHAPE}_mesh.vtk", binary=False)
