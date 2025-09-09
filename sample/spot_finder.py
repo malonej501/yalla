5a0f4788040c3c92cf20b669948cb5f9387a2f92
jmalone@hydra <jamesmalone@tutanota.com>
2025-09-09 15:37:53 +0100

    feat(spot_finder.py): sketched out basic programme structure

--- sample/spot_finder.py
+++ sample/spot_finder.py
@@ -1,9 +1,46 @@
 import pandas as pd
+import numpy as np
 from pwriter import params_to_header
 
+targets = ["D_u","k_prod","k_deg","u_death","A_div"]
+
+def mutate(p_curr, p_i):
+
+    """Change the value of parameter p_i by a random amount"""
+
+    muts = [
+        -np.random.uniform(0, 0.1),
+        -np.random.uniform(0.1, 1),
+        -np.random.uniform(1, 10),
+        np.random.uniform(0, 0.1),
+        np.random.uniform(0.1, 1),
+        np.random.uniform(1, 10)
+    ]
+
+    p_curr["value"][p_i] += np.random.choice(muts)
+
+
+def test_params(p):
+
+    params_to_header(p) # write parameters to header file
+
+    # run simulation
+    # get output
+    # compare output to target
+    # if output is close enough, stop
+    # else, continue
+
 def main():
-    params = pd.read_csv("../params/default.csv")
-    params_to_header(params)
+    p_init = pd.read_csv("../params/default.csv")
+
+    target_idxs = [int(p_init[p_init["param"] == t].index[0]) for t in targets]
+
+    p_curr = p_init.copy()
+    for step in range(0, 100):
+        p_i = np.random.choice(target_idxs) # select target parameter index
+        mutate(p_curr, p_i)
+        test_params(p_curr)
+
 
 if __name__ == "__main__":
     main()
