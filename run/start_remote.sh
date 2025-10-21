bc6d3fdef5efa293b96033f87a932259be589024
malonej501 <jamesmalone@tutanota.com>
2025-10-21 13:19:20 +0100

    feat: added command line argument handling to specify running eggspot.cu or volk.cu

--- run/start_remote.sh
+++ run/start_remote.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # set -e # continue if error
-ssh jmalone@hydra.physics.ox.ac.uk 'source /etc/profile; /mnt/users/jmalone/GitHub/yalla/run/start_model.sh'
+
+POSITIONAL_ARGS=()      # array to hold positional arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v|--version)
+        version="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    *)    # unknown option
+        POSITIONAL_ARGS+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+  esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+ssh jmalone@hydra.physics.ox.ac.uk "source /etc/profile; /mnt/users/jmalone/GitHub/yalla/run/start_model.sh -v ${version}"
 if [ -d "/home/jmalone/GitHub/yalla/run/saves/output" ]; then
     rm -r /home/jmalone/GitHub/yalla/run/saves/output
 fi
