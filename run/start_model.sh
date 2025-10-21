1ca56641d820d05a0d46882798f60705a0cfabc9
jmalone@hydra <jamesmalone@tutanota.com>
2025-10-21 13:18:38 +0100

    feat: added command line argument handling to specify running eggspot.cu or volk.cu and now copying volk files to output

--- run/start_model.sh
+++ run/start_model.sh
@@ -4,9 +4,32 @@
 # cluster parameters
 n_gpus=1
 gpu_type="rtx2080with12gb"  # rtx2080with12gb, rtx3090with24gb
-queue="gpushort"            # gpushort, gpulong
+queue="gpulong"            # gpushort, gpulong
 mem=4                       # allocated memory in GB
 comment="3_mins"            # comment for the job
+
+POSITIONAL_ARGS=()      # array to hold positional arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v|--version)
+        version="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    *)    # unknown option
+        POSITIONAL_ARGS+=("$1") # save it in an array for later
+        shift # past argument
+        ;;
+  esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+echo "Version: ${version:-default}"
+echo "N GPUS: $n_gpus"
+echo "GPU Type: $gpu_type"
+echo "Queue: $queue"
+echo "Memory: $mem GB"
+echo "Comment: $comment"
+
 
 # prepare the environment and load necessary modules
 cd /mnt/users/jmalone/GitHub/yalla/run
@@ -16,12 +39,21 @@ module load cuda
 
 # generate .h file from default parameters
 source ../venv/bin/activate
-python3 ../sample/pwriter.py
+if [[ "$version" -eq "0" ]]; then
+    python3 ../sample/pwriter.py
+else
+    python3 ../sample/pwriter.py -p volk_params.csv
+fi
 deactivate
 
 # compile the model
 echo "Compiling..."
-nvcc -std=c++14 -arch=sm_61 ../examples/eggspot.cu -o exec
+# nvcc -std=c++14 -arch=sm_61 ../examples/eggspot.cu -o exec
+if [[ "$version" -eq "0" ]]; then
+    /usr/local/cuda/bin/nvcc -std=c++17 -arch=sm_61 ../examples/eggspot.cu -o exec
+else
+    /usr/local/cuda/bin/nvcc -std=c++17 -arch=sm_61 ../examples/volk.cu -o exec
+fi
 echo "Compilation time: $SECONDS seconds"
 
 # remove queue lock file if it exists
@@ -46,5 +78,10 @@ done
 echo "Job $job_id has completed."
 
 mv exec-$job_id.out output/exec-$job_id.out 
-cp ../examples/eggspot.cu output/eggspot.cu # copy source code into output directory
-cp ../params/default.csv output/default.csv # copy params.h into output directory
+if [[ "$version" -eq "0" ]]; then
+    cp ../examples/eggspot.cu output/eggspot.cu # copy source code into output directory
+    cp ../params/default.csv output/default.csv # copy params.h into output directory
+else
+    cp ../examples/volk.cu output/volk.cu
+    cp ../params/volk_params.csv output/volk_params.csv 
+fi
